0a211c8a05d72f0d011a9f016968ad7b55c89a9f
Kyler Bragg <braggky@gmail.com>
2024-04-21 21:01:16 -0400

    Updated Device information output

--- Ember/src/Ember/Platform/Vulkan/VulkanContext.cpp
+++ Ember/src/Ember/Platform/Vulkan/VulkanContext.cpp
@@ -12,13 +12,13 @@ void VulkanContext::Init()
 {
     m_Device = std::make_unique<VulkanDevice>(m_WindowHandle);
 
-    EM_CORE_INFO("Device Info:");
-    EM_CORE_INFO("  Vendor: {0}", m_Device->GetDeviceVendor());
-    EM_CORE_INFO("  Name: {0}", m_Device->GetProperties().deviceName);
-    EM_CORE_INFO("  Type: {0}", m_Device->GetDeviceType());
+    EM_CORE_INFO("Vulkan Info:");
     EM_CORE_INFO("  API Version: {0}.{1}.{2}", VK_VERSION_MAJOR(m_Device->GetProperties().apiVersion),
                  VK_VERSION_MINOR(m_Device->GetProperties().apiVersion),
                  VK_VERSION_PATCH(m_Device->GetProperties().apiVersion));
+    EM_CORE_INFO("  Device Vendor: {0}", m_Device->GetDeviceVendor());
+    EM_CORE_INFO("  Device Name: {0}", m_Device->GetProperties().deviceName);
+    EM_CORE_INFO("  Device Type: {0}", m_Device->GetDeviceType());
     EM_CORE_INFO("  Driver Version: {0}", m_Device->GetProperties().driverVersion);
 }
 
--- Ember/src/Ember/Platform/Vulkan/VulkanDevice.cpp
+++ Ember/src/Ember/Platform/Vulkan/VulkanDevice.cpp
@@ -21,7 +21,7 @@ void VulkanDevice::CreateInstance()
     appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName   = "Ember Engine";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName        = "No Engine";
+    appInfo.pEngineName        = "Ember";
     appInfo.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion         = VK_API_VERSION_1_0;
 
